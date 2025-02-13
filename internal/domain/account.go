9d832dd147cf9f37ddf9f3b1050760c9f253b174
edkirill <edkirill@yandex.ru>
2025-02-13 15:22:09 +0300

    add account domain + cryptopass pkg

---
+++ internal/domain/account.go
@@ -0,0 +1,43 @@
+package domain
+
+import (
+	"github.com/google/uuid"
+	"github.com/m11ano/avito-shop/pkg/cryptopass"
+)
+
+type Account struct {
+	ID           uuid.UUID
+	Username     string
+	PasswordHash string
+}
+
+func (a *Account) GeneretePasswordHash(password string) error {
+	salt, err := cryptopass.GenerateSalt()
+	if err != nil {
+		return err
+	}
+
+	hash, err := cryptopass.HashPasswordArgon2(password, salt)
+	if err != nil {
+		return err
+	}
+
+	a.PasswordHash = hash
+
+	return nil
+}
+
+// New account with text password, hash will be generated automatically
+func NewAccount(username string, password string) (*Account, error) {
+	account := &Account{
+		ID:       uuid.New(),
+		Username: username,
+	}
+
+	err := account.GeneretePasswordHash(password)
+	if err != nil {
+		return nil, err
+	}
+
+	return account, nil
+}
